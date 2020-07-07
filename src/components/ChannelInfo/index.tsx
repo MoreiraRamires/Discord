54a1158e8666871a8e81913fd36efb581cc8af47
MoreiraRamires <62817742+MoreiraRamires@users.noreply.github.com>
2020-07-07 10:15:08 -0300

    r2d2

--- src/components/ChannelInfo/index.tsx
+++ src/components/ChannelInfo/index.tsx
@@ -5,13 +5,15 @@ export default function Layout (){ return <div> Oi mundo </div>}; */
 
 import { HastagIcon, Title, Separator, Description, Container } from "./styles"
 
+import Robo from "../../assents/r2.png"
+
 
 const ChannelInfo: React.FC = () => {
   return (
     <Container>
       <HastagIcon />
       <Title > ChatLivre</Title>
-      <Separator />
+      <Separator> <img src={Robo} alt="R2-D2" /></Separator>
       <Description> Canal aberto para conversas</Description>
 
 
--- src/components/ChannelInfo/styles.ts
+++ src/components/ChannelInfo/styles.ts
@@ -1,24 +1,47 @@
 import styled from "styled-components";
-import { Chat } from 'styled-icons/material';
+import { Hashtag } from 'styled-icons/heroicons-outline';
 
 export const Container = styled.div`
 grid-area:CI;
+display:flex;
+align-items:center;
+
+padding: 0 17px;
+background-color: var(--secondary);
+box-shadow: rgba(0,0,0,0.2) 0px 1px 0px 0px;
+z-index:2;
 
 
 `;
-export const HastagIcon = styled.(Chat)`
+export const HastagIcon = styled(Hashtag)`
+width:24px;
+height:24px;
+color:var(--symbol)
 
 `;
 
 export const Title = styled.h1`
+  margin-left:9px;
 
+  font-size:16px;
+  font-weight:bold;
+  color:var(--white);
 `;
 
 export const Separator = styled.div`
 
+
+>img{ 
+  width:28px;
+  height:28px;
+  
+  margin-left:10px}
 `;
 
 
-export const Description = styled.h3`
+export const Description = styled.h5`
+margin-left:9px;
 
+font-size:14px;
+color:var(--white);
 `;
